d9bc1d874c1519b72aae49249b724a8e6237d94a
Rob Ballantyne <raballantyne@gmail.com>
2019-07-12 22:36:52 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,15 @@
+
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def position_taken?(board, index)
+  taken = nil
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    taken = false
+  else 
+   taken = true
+  end
+  taken
+end
