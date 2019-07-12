6f18d2050c5c881458e547931a2975f03edecbfc
Rob Ballantyne <raballantyne@gmail.com>
2019-07-12 22:38:43 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,6 +10,9 @@ def input_to_index(user_input)
   input = user_input.to_i - 1
 end
 
+def move(board, input, char="X")
+  board[input] = char
+  
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
